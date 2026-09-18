7802a35a9b446bdd6e1cc9738bc90bad0c598da0
danidawsuperior <danidawsuperior@gmail.com>
2026-09-18 10:32:23 +0200

    Add files via upload

---
+++ primero.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <pre>
+        PEPE            MANOLO
+        LUIS
+    </pre>
+    <h1>HOLA</h1>
+    <?php
+        echo " Mundo <br><br>";
+        $numero = 1;
+        $numero++;
+        $numero++;
+        echo $numero;
+        echo " Mundo <br><br>" .$numero;
+        echo $numero+4;
+    ?>
+</body>
+</html>
