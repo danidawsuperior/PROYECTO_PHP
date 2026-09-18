f8c52d3baee41aabb952a9d3bab4af2fe7bf4e0b
Daniel <danidawsuperior@gmail.com>
2026-09-18 10:38:41 +0200

    Nueva carpeta

---
+++ segundo.php
@@ -0,0 +1,13 @@
+<?php
+echo "Hola mundo <br>";
+$valor = 33;
+$valor++;
+echo $valor;
+echo " El número es $valor <br>";
+echo ' El número es $valor <br>';
+
+echo " El número es" .$valor."<br>";
+
+for ($i = 0; $i < 10; $i++) {
+    echo "$i <br>";
+}
